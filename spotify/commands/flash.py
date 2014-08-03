3c3859918ab6486cab3c563708ce40902e52c0f1
Hendrik Hagendorn <netrik@netrik.de>
2014-08-03 16:09:42 +0200

    Update PingFlash2 handler, use spotlite, spotify.py and nodespotifyweb api

--- spotify/commands/flash.py
+++ spotify/commands/flash.py
@@ -1,5 +1,5 @@
+import requests, json
 from spotify.commands.base import Command
-from spotify.commands.flash_key import FLASH_KEY
 
 import logging
 
@@ -8,18 +8,37 @@ log = logging.getLogger(__name__)
 
 class PingFlash2(Command):
     def process(self, ping):
-        ping = ping.split(' ')
-        pong = "undefined 0"
+        spotlite_host = "http://spotlite.cloudapp.net/api/pong/"
+        spotifypy_host = "http://spflash.herokuapp.com/generic/get?ping="
+        nodespotifyweb_host = "http://ping-pong.spotify.nodestuff.net/"
 
-        if len(ping) >= 20:
-            result = []
-
-            for idx, code in FLASH_KEY:
-                val = int(ping[idx])
-
-                result.append(str(val ^ code if type(code) is int else code[val]))
-
-            pong = ' '.join(result)
+        # Try spotlite api
+        try:
+            req = requests.get(spotlite_host + ping.replace(' ', '_'), timeout=5)
+            pong = req.content.strip('"')
+            if (pong == "None") or (req.status_code != 200):
+                raise Exception()
+        except Exception, err:
+            log.debug('spotlite API seems down. Now trying spotify.py API. ' + str(err))
+            # Try spotify.py api
+            try:
+                req = requests.get(spotifypy_host + ping, timeout=5)
+                pong = req.content
+                if req.status_code != 200:
+                    raise Exception()
+            except Exception:
+                log.debug('spotify.py API seems down. Now trying node-spotify-web API.')
+                # Try node-spotify-web api
+                try:
+                    req = requests.get(nodespotifyweb_host + ping.replace(' ', '-'), timeout=5)
+                    json = json.loads(req.content)
+                    if (json['status'] != 100) or (req.status_code != 200):
+                        raise Exception()
+                    pong = json['pong'].replace('-', ' ')
+                except Exception:
+                    log.debug('node-web-spotify API seems down.')
+                    log.debug('All available APIs seemed down.')
+                    return
 
         log.debug('received ping %s, sending pong: %s' % (ping, pong))
 
